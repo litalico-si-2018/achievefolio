2b21ecff4f3caf8a0ebfb3e9ce6dbeb246b8293b
kthatoto <kthatoto@gmail.com>
2018-08-12 01:56:21 +0900

    Added user components

---
+++ src/pages/User.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.user
+  Nav.nav(:user="user")
+  .content
+</template>
+<script>
+import Nav from '@/components/Nav'
+export default {
+  components: { Nav },
+  data () {
+    return {
+      user: {}
+    }
+  },
+  created () {
+    this.user = {
+      id: 1,
+      name: '山田',
+      icon: 'yamada.jpg',
+      role: 'general'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.nav {
+  width: 250px;
+  float: left;
+  height: 100vh;
+}
+.content {
+  width: calc(100% - 300px);
+  float: left;
+}
+</style>
---
+++ src/pages/Users.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.users
+  Nav.nav(:user="user")
+  .content
+</template>
+<script>
+import Nav from '@/components/Nav'
+export default {
+  components: { Nav },
+  data () {
+    return {
+      user: {},
+      users: []
+    }
+  },
+  created () {
+    // api(`${process.env.API_ENDPOINT}/pages/mypage`)
+    this.user = {
+      id: 1,
+      name: '山田',
+      icon: 'yamada.jpg',
+      role: 'general'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.nav {
+  width: 250px;
+  float: left;
+  height: 100vh;
+}
+.content {
+  width: calc(100% - 300px);
+  float: left;
+}
+</style>
