e263b0f65c59ffcb3cdd8b4318b2d0608211ee3a
kthatoto <kthatoto@gmail.com>
2018-08-12 06:47:16 +0900

    Added calendar component

---
+++ src/components/Calendar.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.calendar
+  .year
+    .week(v-for="i in 26")
+      .date(v-for="j in 7")
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss" scoped>
+.year {
+  width: 150px;
+}
+.week {
+  height: 15px;
+  padding-top: -5px;
+  margin-bottom: 2px;
+}
+.date {
+  width: 15px;
+  height: 15px;
+  background-color: #ccc;
+  margin: 0 1px;
+  float:left;
+}
+</style>
