ffb06b6f7513d232778bac5ce7d2516fc7d3bf53
kthatoto <kthatoto@gmail.com>
2018-08-12 06:47:27 +0900

    Added achievements components

---
+++ src/components/Achievement.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.achievement
+  icon(:name="typeToName[type]")
+</template>
+<script>
+export default {
+  props: ['type'],
+  data () {
+    return {
+      "typeToName": {
+        string_count: "align-left"
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.achievement {
+  width: 50px;
+  height: 50px;
+}
+</style>
---
+++ src/components/AchievementBoard.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.achievementBoard
+  Achievement(v-for="achievement in achievements")
+</template>
+<script>
+import Achievement from '@/components/Achievement'
+export default {
+  components: { Achievement },
+  props: ['achievements']
+}
+</script>
+<style lang="scss" scoped>
+.achievementBoard {
+}
+</style>
