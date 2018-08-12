c9a619500a2f2ed401f2765d5f86ad57496e7418
kthatoto <kthatoto@gmail.com>
2018-08-12 12:39:36 +0900

    Call api

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,13 +1,24 @@
 <template lang="pug">
 .posts
+  .posts__item(v-for="post in posts" :key="post.id")
+
 </template>
 <script>
+import Avatar from '@/components/Avatar'
+
 export default {
+  components: { Avatar },
   props: ['posts'],
   created () {
-    console.log(this.posts)
+  },
+  methods: {
   }
 }
 </script>
 <style lang="scss" scoped>
+.posts {
+  &__item {
+    background-color: white;
+  }
+}
 </style>
--- src/pages/Mypage.vue
+++ src/pages/Mypage.vue
@@ -9,7 +9,7 @@
       Posts(:posts="posts")
 </template>
 <script>
-// import api from '@/utils/Api'
+import api from '@/utils/Api'
 import Nav from '@/components/Nav'
 import Calendar from '@/components/Calendar'
 import AchievementBoard from '@/components/AchievementBoard'
@@ -27,13 +27,16 @@ export default {
     }
   },
   created () {
-    // api(`${process.env.API_ENDPOINT}/pages/mypage`)
-    this.user = {
-      id: 1,
-      name: '山田',
-      icon: 'yamada.jpg',
-      role: 'general'
-    }
+    api('GET',
+      `${process.env.API_ENDPOINT}/pages/mypage`,
+      {}
+    ).then(response => {
+      console.log(response.data)
+      this.user = response.data.user
+      this.drops = response.data.drops
+      this.achievements = response.data.achievements
+      this.posts = response.data.posts
+    })
     this.achievements = Array.from(
       new Array(36), (_, index) => index + 1
     ).map(i => {
@@ -46,30 +49,6 @@ export default {
         description: '文字数200文字以上の投稿をする'
       }
     })
-    this.posts = [
-      {
-        id: 1,
-        comments: [
-          {
-            id: 1,
-            type: 'post_content',
-            body: '頑張った！'
-          },
-          {
-            id: 2,
-            type: 'comment',
-            body: '何を頑張ったの？'
-          },
-          {
-            id: 3,
-            type: 'comment',
-            body: 'データ入力をミスなく完璧に頑張った！'
-          }
-        ],
-        approved: false,
-        mine: true
-      }
-    ]
   }
 }
 </script>
